8c3ae4f6eb06d9705c16ad6fbe2265531671ddfc
loganathan <loganathan.ms@gmail.com>
2014-03-02 02:05:22 +0530

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 GdriveApiIntegration::Application.routes.draw do
-  get "drive_buttons/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root "drive_buttons#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
