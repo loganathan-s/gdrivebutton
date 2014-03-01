553e2c917a81dfc267c8a4dacbcda3ad430e4006
loganathan <loganathan.ms@gmail.com>
2014-03-02 02:06:35 +0530

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ GdriveApiIntegration::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root "drive_buttons#index"
+   root "drive_buttons#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
